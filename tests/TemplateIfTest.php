eb0339b9a5bcd765990382389c82a62236635189
Joao Gilberto Magalhaes <joao@byjg.com.br>
2024-09-16 17:13:21 -0500

    Added some tests

--- tests/TemplateIfTest.php
+++ tests/TemplateIfTest.php
@@ -47,8 +47,17 @@ class TemplateIfTest extends TestCase
         $template = new \ByJG\JinjaPhp\Template("{% if var1 == 'abc' && !(var2 == 123) %}Show result of {{ var3 }}{% else %}Show nothing{% endif %}");
         $this->assertEquals("Show nothing", $template->render(['var1' => 'abc', 'var2' => 123, 'var3' => 456]));
 
-        // $template = new \ByJG\JinjaPhp\Template("{% if true %}true{% elseif true %}false{% endif %}");
-        // $this->assertEquals("true", $template->render());
+        $template = new \ByJG\JinjaPhp\Template("{% if var1.type == 'test' %}true{%else%}false{% endif %}");
+        $this->assertEquals("true", $template->render(['var1' => ['type' => 'test']]));
+
+//        $template = new \ByJG\JinjaPhp\Template("{% if var1.type == 'test(1)' %}true{%else%}false{% endif %}");
+//        $this->assertEquals("true", $template->render(['var1' => ['type' => 'test(1)']]));
+
+        $template = new \ByJG\JinjaPhp\Template("{% if 'test' in var1.type %}true{%else%}false{% endif %}");
+        $this->assertEquals("true", $template->render(['var1' => ['type' => 'test(1)']]));
+
+//         $template = new \ByJG\JinjaPhp\Template("{% if true %}true{% elseif true %}false{% endif %}");
+//         $this->assertEquals("true", $template->render());
 
         // $template = new \ByJG\JinjaPhp\Template("{% if false %}true{% elseif true %}false{% endif %}");
         // $this->assertEquals("false", $template->render());
